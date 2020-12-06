72ac75369bfd6e9007f220f62055c6ee27dee71b
Mauricio Chirino <mauricio.chirino@pedidosya.com>
2020-12-06 19:34:42 -0300

    Add app's base scaffolding

---
+++ R&M/R&M/Delivery/MainListViewController.swift
@@ -0,0 +1,16 @@
+//
+//  MainListViewController.swift
+//  R&M
+//
+//  Created by Mauricio Chirino on 06/12/20.
+//
+
+import UIKit
+
+final class MainListViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+    }
+}
+
---
+++ R&M/R&M/Setup/AppDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.swift
+//  R&M
+//
+//  Created by Mauricio Chirino on 06/12/20.
+//
+
+import UIKit
+
+@main
+final class AppDelegate: UIResponder, UIApplicationDelegate {
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        return true
+    }
+}
+
