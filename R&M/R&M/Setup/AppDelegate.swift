51246953affc8f5d8d91691ac53b3fa097858c0b
Mauricio Chirino <1657723+mchirino89@users.noreply.github.com>
2023-01-09 12:21:32 -0300

    Remove scene delegate

--- R&M/R&M/Setup/AppDelegate.swift
+++ R&M/R&M/Setup/AppDelegate.swift
@@ -9,8 +9,22 @@ import UIKit
 
 @main
 final class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+    let globalCache = CacheStore()
+
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        let rootCoordinator = MainCoordinator(characterRepo: CharacterStoredRepo(), cache: globalCache)
+        rootCoordinator.start()
+
+        buildMainWindow(with: rootCoordinator.rootViewController)
+
         return true
+    }
+
+    private func buildMainWindow(with rootController: UINavigationController?) {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = rootController
+        window?.makeKeyAndVisible()
     }
 }
